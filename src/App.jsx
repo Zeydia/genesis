49572885803320e1a3713786610729eac1ab3f56
Zeydia <douseydiallo2002@gmail.com>
2025-01-21 13:36:57 +0000

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home from './pages/Home/Home'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-3xl underline">Hello</h1>
+    </>
+  )
+}
+
+export default App
