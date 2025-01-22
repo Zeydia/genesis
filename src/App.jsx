2e6162b8d5ebc965d4d116354e237a4bb7bb624b
Zeydia <douseydiallo2002@gmail.com>
2025-01-22 12:58:02 +0000

    init routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import Home from './pages/Home/Home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Page404 from './Page404'
+import Dashboard from './admin/pages/Dashboard/Dashboard'
+import Layout from './pages/Layout/Layout'
 
 function App() {
   
 
   return (
-    <>
-      <h1 className="text-3xl underline">Hello</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />} >
+          <Route index element={<Home />} />
+        
+        </Route>
+        <Route path="/dashboard" element={<Dashboard />}>
+
+        </Route>
+        <Route path="*" element={<Page404 />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
